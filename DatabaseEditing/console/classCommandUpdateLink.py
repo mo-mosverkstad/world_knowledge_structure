31468c226a43661f3dbbf8561a5221f8d0cfd730
mo-mosverkstad <mo.mosverkstad@gmail.com>
2023-03-19 11:41:39 +0100

    Add class to handle problem
    Add class to handle problem and related error message

--- DatabaseEditing/console/classCommandUpdateLink.py
+++ DatabaseEditing/console/classCommandUpdateLink.py
@@ -4,5 +4,14 @@ from .classCargo import Cargo
 class CommandUpdateLink(Command):
     def run(self, cargo:Cargo) -> Cargo:
         values = cargo.values.strip().split(" ")
+        if len(values) != 2:
+            print("Problem (parameters) Updatelink/move command requires 2 parameters")
+            return
+        if not values[0].isnumeric():
+            print("Problem (parameters) Childid parameter <childid> is not a number")
+            return
+        if not values[1].isnumeric():
+            print("Problem (parameters) New parentid parameter <parentid> is not a number")
+            return
         if len(values) >= 2:
             cargo.console.system.update_link(values[0], values[1])

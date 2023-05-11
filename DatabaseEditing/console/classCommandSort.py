6a4c4c19c7fdc37205ad84f6e27e7db005b6803c
mo-mosverkstad <mo.mosverkstad@gmail.com>
2023-05-11 20:27:10 +0200

    Json backup
    Json backup

--- DatabaseEditing/console/classCommandSort.py
+++ DatabaseEditing/console/classCommandSort.py
@@ -7,4 +7,10 @@ class CommandSort(Command):
         if len(values) != 2:
             print("Problem: (parameter) Sort command reqires 2 parameters")
             return
+        if not values[0].isdigit():
+            print("Problem: (parameter) Childid parameter <childid> is not a number")
+            return
+        if not values[1].isdigit():
+            print("Problem: (parameter) New sortid parameter <sortid> is not a number")
+            return
         cargo.console.system.sort(values[0], values[1])

f6f6cf383c976fd37cdebc75226ead66be5ee7d6
mo-mosverkstad <mo.mosverkstad@gmail.com>
2023-09-05 12:52:45 +0200

    Improve the performace again
    Improve the performance of loading time, use two plain SQL to replace the join SQL statement.

---
+++ Deployment php/Deployment.php local/htdocs.20230905/indexlocal.php
@@ -0,0 +1,75 @@
+<html>
+    <?php
+        function webLoad($requestURL, $scriptName){
+            $url = $requestURL;
+            $ch = curl_init();  
+            curl_setopt($ch,CURLOPT_URL,$url);
+            curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+            $page = curl_exec($ch);
+            curl_close($ch);
+            echo "<script>var $scriptName = $page;</script>";
+        }
+    ?>
+    <head>
+        <link rel="stylesheet" href="style.css" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    </head>
+    <body><center>
+        <table width="950" height="65"><tr>
+            <td width="180">Mosverkstad logo</td>
+            <td width="400"><input id="search"/></td>
+            <td width="80"><button id="submit">Search</button></td>
+            <td>&nbsp;</td>
+			<td width="90"><button>Log in</button></td>
+        </tr></table>
+        <img width="950" src="https://uploads-ssl.webflow.com/6123b7f3a062a25a8b4d4119/6287bdb869b482145f371e15_access.png">
+        <table width="950" id="VisualTable"></table>
+		<?php
+                        // temporarily remove for high performance 1/2
+			// webLoad("https://webcloud.sl.se/api/v2/departures?mode=departures&origPlaceId=QT0xQE89QnJvbW1hcGxhbiAoU3RvY2tob2xtKUBYPTE3OTM3ODY3QFk9NTkzMzgyOThAVT03NEBMPTMwMDEwOTEwOUBCPTFAcD0xNjU0NTYzNjE1QA%3D%3D&origSiteId=9109&desiredResults=3&origName=Brommaplan+%28Stockholm%29", "trafficBromma");
+			// webLoad("https://webcloud.sl.se/api/v2/departures?mode=departures&origPlaceId=QT0xQE89T2RlbnBsYW4gKFN0b2NraG9sbSlAWD0xODA0OTA5OUBZPTU5MzQyOTAxQFU9NzRATD0zMDAxMDkxMTdAQj0xQHA9MTY1NTI1NDgxNEA%3D&origSiteId=9117&desiredResults=3&origName=Odenplan+%28Stockholm%29", "trafficOdenplan");
+			// webLoad("https://webcloud.sl.se/api/v2/departures?mode=departures&origPlaceId=QT0xQE89VG9yc3BsYW4gKFN0b2NraG9sbSlAWD0xODAzMjkzNkBZPTU5MzQ1NDE3QFU9NzRATD0zMDAxMDEwNjVAQj0xQHA9MTY1NTI1NDgxNEA%3D&origSiteId=1065&desiredResults=3&origName=Torsplan+%28Stockholm%29", "trafficTorsplan");
+			// webLoad("https://www.yr.no/api/v0/locations/2-2673730/forecast", "weather");
+		?>
+		<?php
+			function databaseLoad(){
+			    $servername = "localhost";
+			    $username = "root";
+			    $password = "mysql";
+			    $dbname = "temp";
+				$conn = mysqli_connect($servername, $username, $password, $dbname);
+				mysqli_set_charset($conn, "utf8");
+				if (!$conn) {
+					die("Connection failed: " . mysqli_connect_error());
+				}
+				
+				$resultItems = mysqli_query($conn, "SELECT id, item FROM nameTable;");
+				if (mysqli_num_rows($resultItems) <= 0){
+					echo "<script>var dbItems = []</script>";
+					echo "<script>var dbRelationship = []</script>";
+					return;
+				}
+				echo "<script>var dbItems = {";
+				while($row = mysqli_fetch_assoc($resultItems)) {
+					echo $row["id"] . ": '" . addslashes($row["item"]) . "',";
+				}
+				echo "}</script>";
+				
+				$resultRelationship = mysqli_query($conn, "SELECT * FROM relationship;");
+				if (mysqli_num_rows($resultRelationship) <= 0){
+					echo "<script>var dbRelationship = []</script>";
+					return;
+				}
+				echo "<script>var dbRelationship = [";
+				while($row = mysqli_fetch_assoc($resultRelationship)) {
+					echo "{parentId: " . $row["parentId"] . ", childId: " . $row["childId"] . "},";
+				}
+				echo "]</script>";				
+				mysqli_close($conn);
+				return;
+			}
+			databaseLoad()
+		?>
+        <script src="main.js"></script>
+    </center></body>
+</html>
---
+++ Deployment php/Deployment.php local/htdocs.20230905/main.js
@@ -0,0 +1,158 @@
+console.log(dbItems);
+console.log(dbRelationship)
+
+class KnowledgeData {
+    constructor(){
+        this.mapIdToIndex = {};
+        this.data = [];
+        this.rootIndex = 0;
+    }
+    
+    load(dbItems, dbRelationship) {
+        for (let row of dbRelationship) {
+            let parentId = row['parentId'];
+            let childId = row['childId'];
+            let parentIndex = this.getIndex(parentId, dbItems[parentId]);
+            let childIndex = this.getIndex(childId, dbItems[childId]);
+            this.addChildToParent(parentIndex, childIndex);
+        }
+    }
+    
+    getIndex(itemId, itemName) {
+        let dataIndex = 0;
+        if (!(itemId in this.mapIdToIndex)) {
+            dataIndex = this.createToData(itemId, itemName);
+            this.mapIdToIndex[itemId] = dataIndex;
+        } else {
+            dataIndex = this.mapIdToIndex[itemId];
+        }
+        return dataIndex;
+    }
+    
+    createToData(itemId, itemName) {
+        var itemData = {};
+        itemData['id'] = itemId;
+        itemData['name'] = itemName;
+        itemData['nodes'] = [];
+        return this.data.push(itemData) - 1;
+    }
+    
+    addChildToParent(parentIndex, childIndex) {
+        this.data[parentIndex].nodes.push(childIndex);
+    }
+    
+    setRootIndex(itemId, itemName, children){
+        this.rootIndex = this.createToData(itemId, itemName);
+        for (let child of children) {
+            this.data[this.rootIndex].nodes.push(child);
+        }
+    }
+}
+
+var kd = new KnowledgeData();
+kd.load(dbItems, dbRelationship);
+kd.setRootIndex(-1, 'Root', [0]);
+
+console.log(kd.data);
+
+class Stack extends Array{
+    constructor(items){
+        super();
+        for (var item of items){
+            this.push(item);
+        }
+    }
+    
+    peek(){
+        return this[this.length - 1];
+    }
+}
+
+class ViewTable {
+    constructor(tableDimentions = [], segmentClassName = "") {
+        this.HTMLElement = document.getElementById("VisualTable");
+        this.tableDimentions = tableDimentions;
+        this.segmentClassName = segmentClassName;
+    }
+    
+    _create_segment(segmentElement, name, width){
+        let segment = document.createElement(segmentElement);
+        segment.innerHTML = name;
+        segment.style.width = width;
+        segment.className = this.segmentClassName;
+        return segment;
+    }
+    
+    add(rowNames, segmentElement = "td") {
+        let row = this.HTMLElement.insertRow();
+        for (let index = 0; index < rowNames.length; index++) {
+            row.appendChild(this._create_segment(segmentElement, rowNames[index], this.tableDimentions[index]));
+        }
+    }
+    
+    clear() {
+        this.HTMLElement.innerHTML = "";
+    }
+    
+    click(rowIndex, callback, cargo) {
+        this.get(rowIndex).cargo = cargo;
+        this.get(rowIndex).addEventListener("click", callback);
+    }
+    
+    get(rowIndex) {
+        if (rowIndex < 0) {
+            rowIndex = this.HTMLElement.rows.length + rowIndex;
+        }
+        return this.HTMLElement.rows[rowIndex];
+    }
+}
+
+class DataViewTable extends ViewTable {
+    constructor(data, rootIndex, tableDimentions = [], segmentClassName = "") {
+        super(tableDimentions, segmentClassName);
+        this.data = data;
+        this.idPath = new Stack([rootIndex]);
+        this.namePath = new Stack([this.data[rootIndex].name]);
+    }
+    
+    _drawHeader(){
+        this.add(["ID", "Contents " + this.namePath.join("/")], "th");
+        this.click(-1, this.ascend, this);
+    }
+    
+    _drawNodeRow(index){
+        let node = this.data[this.idPath.peek()].nodes[index];
+        if (typeof node == "number") {
+            this.add([index + 1, this.data[node].name]);
+            this.click(-1, this.descend, { pushId: node, pushName: this.data[node].name, table: this });
+            return;
+        }
+        this.add([index + 1, node])
+    }
+    
+    draw() {
+        this.clear();
+        this._drawHeader();
+        for (let index = 0; index < this.data[this.idPath.peek()].nodes.length; index++) {
+            this._drawNodeRow(index);
+        }
+    }
+    
+    ascend() {
+        if (this.cargo.idPath.length > 1) {
+            this.cargo.idPath.pop();
+            this.cargo.namePath.pop();
+            this.cargo.draw();
+        }
+    }
+    
+    descend() {
+        this.cargo.table.idPath.push(this.cargo.pushId);
+        this.cargo.table.namePath.push(this.cargo.pushName);
+        this.cargo.table.draw();
+    }
+}
+
+
+var v = new DataViewTable(kd.data, kd.rootIndex, [70, NaN], "view")
+v.draw()
